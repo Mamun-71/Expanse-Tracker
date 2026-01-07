81903b5687670b909665dd6ee4977dbda459370a
Al-Mamun Salauddin <almamunsalauddin1234@gmail.com>
2026-01-07 11:54:08 +0600

    expense tracker

--- routes/api.php
+++ routes/api.php
@@ -4,12 +4,14 @@ use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\ExpenseController;
 use Illuminate\Support\Facades\Route;
 
-// Public
-Route::post('/register', [AuthController::class, 'register']);
-Route::post('/login', [AuthController::class, 'login']);
+//// Public
+//Route::post('/register', [AuthController::class, 'register']);
+//Route::post('/login', [AuthController::class, 'login']);
+//
+//// Protected
+//Route::middleware('auth:sanctum')->group(function () {
+//    Route::post('/logout', [AuthController::class, 'logout']);
+//});
 
-// Protected
-Route::middleware('auth:sanctum')->group(function () {
-    Route::post('/logout', [AuthController::class, 'logout']);
-    Route::apiResource('expenses', ExpenseController::class);
-});
+
+Route::apiResource('expenses', ExpenseController::class);
